24e48f2ce53510eb5a522b3befb10dd4b6de175b
Joselu <jose.gallardo994@gmail.com>
2021-10-02 17:07:25 +0200

    Finished components layout

--- angular-lab/src/app/components/profile/profile.component.ts
+++ angular-lab/src/app/components/profile/profile.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthenticationService } from "src/app/services/authentication.service";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.css"],
 })
 export class ProfileComponent implements OnInit {
+  constructor(private auth: AuthenticationService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  getUsername(): string {
+    return this.auth.getUsername();
   }
-
 }
